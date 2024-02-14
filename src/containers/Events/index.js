cde2acfecb100dd2284a1d12e2049835b96f34c4
Fanny <lakrame@hotmail.com>
2024-02-14 09:41:30 +0100

    version finale

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,26 +13,25 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-
-  const typeList = new Set(data?.events.map((event) => event.type));
-
-  // Fonction pour filtrer les événements en fonction de la catégorie sélectionnée
   const filteredEvents = (
-    (!type ? data?.events : data?.events.filter((event) => event.type === type)) || []
+    (!type 
+      ? data?.events 
+      : data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
-    if ((currentPage - 1) * PER_PAGE <= index && PER_PAGE * currentPage > index) {
+    if (
+      (currentPage - 1) * PER_PAGE <= index && 
+      PER_PAGE * currentPage > index
+      ) {
       return true;
     }
     return false;
   });
-
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-
+  const typeList = new Set(data?.events.map((event) => event.type));// changement 
   return (
     <>
       {error && <div>An error occurred</div>}
--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Slider from "./index";
 import { api, DataProvider } from "../../contexts/DataContext";
 
@@ -40,5 +40,27 @@ describe("When slider is created", () => {
     await screen.findByText(
       "Oeuvre à la coopération entre le secteur public et le privé."
     );
+  });
+});
+
+describe("Slider component", () => {
+  it("displays images in descending order", async () => {
+    jest.spyOn(api, "loadData").mockResolvedValue(data);
+
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+
+    await waitFor(() => screen.getAllByAltText("forum"));
+
+    const images = screen.getAllByAltText("forum");
+
+    for (let i = 0; i < images.length - 1; i++) {
+      const currentImageDate = new Date(data.focus[i].date);
+      const nextImageDate = new Date(data.focus[i + 1].date);
+      expect(currentImageDate >= nextImageDate).toBe(true);
+    }
   });
 });
